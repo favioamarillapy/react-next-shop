a107fa8345a8b869c5b509f006cd78fd90848ef9
favioamarillapy <53306199+favioamarillapy@users.noreply.github.com>
2022-04-26 19:00:09 -0400

    Showing second product image when mouse enter in product card

--- components/product/ProductCard.js
+++ components/product/ProductCard.js
@@ -1,19 +1,43 @@
-import { Card, CardActionArea, CardMedia, Grid } from '@mui/material'
+import { Box, Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
+import { useMemo, useState } from 'react'
 
-export const ProductCard = ({ title, slug, images }) => {
+export const ProductCard = ({ title, slug, price, images }) => {
+
+  const [isHovered, setIsHovered] = useState(false)
+
+  const productImage = useMemo(() => {
+    return isHovered ? 'products/' + images[1] : 'products/' + images[0]
+  }, [isHovered, images])
+
+
+
   return (
-    <Grid item xs={6} sm={4} key={slug}>
+    <Grid
+      item
+      xs={6}
+      sm={4}
+      key={slug}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+
       <Card>
         <CardActionArea>
           <CardMedia
+            className="fadeIn"
             component='img'
-            image={'products/' + images[0]}
+            image={productImage}
             alt={title}
           >
-
           </CardMedia>
         </CardActionArea>
       </Card>
+
+      <Box sx={{ mt: 1 }} className="fadeIn">
+        <Typography fontWeight={700}>{title}</Typography>
+        <Typography fontWeight={500} >$ {price}</Typography>
+      </Box>
+
     </Grid>
   )
 }
