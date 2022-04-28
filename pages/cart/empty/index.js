6bd0bebf2b4116ef3400517fee4832e48b75d9fa
favioamarillapy <53306199+favioamarillapy@users.noreply.github.com>
2022-04-28 14:31:10 -0400

    Created cart empty page

---
+++ pages/cart/empty/index.js
@@ -0,0 +1,19 @@
+import NextLink from 'next/link'
+import { RemoveShoppingCartOutlined } from '@mui/icons-material'
+import { Box, Link, Typography } from '@mui/material'
+import { ShopLayout } from '../../../components/layouts'
+
+const CartEmpty = () => {
+  return (
+    <ShopLayout>
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: { xs: 'column', sm: 'column' } }}>
+
+        <RemoveShoppingCartOutlined sx={{ fontSize: 100, color: '#c2c2c2' }} />
+        <Typography variant='h4' fontWeight={200} sx={{ color: '#c2c2c2' }}>You&apos;re cart is empty</Typography>
+
+      </Box>
+    </ShopLayout>
+  )
+}
+
+export default CartEmpty
